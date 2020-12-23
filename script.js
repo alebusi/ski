d877c7eb893864bad43f2a9666afe8652a191adf
alebusi <allebusi@gmail.com>
2020-12-23 10:44:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -85,7 +85,7 @@ function drawPista() {
     document.getElementById("pista").style.left = pistaX+"px";
     if (Math.abs(pistaY) > 6400) {
 	try {clearInterval(start_game);}
-	catch{}
+	catch(err){}
     }
 }
 
